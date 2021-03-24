1e9802769f9f934c2142b4732994677b2606c777
Your Name <you@example.com>
2021-03-23 23:48:26 -0500

    once more

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ class Config(object):
     """ Extra comment"""
     DEBUG = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'Som3$ec5etK*y'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://user:password@localhost/project1'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE') or 'postgresql://user:password@localhost/project1'
     SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
     UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER') or './uploads'
 
